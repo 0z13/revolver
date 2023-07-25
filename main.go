f49f7da48523da5bc85195e325e650ce552f0cbd
Jonathan Zielinski <jozi@dbc.dk>
2023-07-25 23:57:47 +0200

    zz

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 
-func lookup(qname string, qtype dnspacket.QueryType) ([]byte, error){
+func lookup(qname string, qtype dnspacket.QueryType, id uint16) ([]byte, error){
     udpAddr, _ := net.ResolveUDPAddr("udp", "8.8.8.8:53")
 
 	qs := []dnspacket.DNSQuestion{
@@ -26,7 +26,7 @@ func lookup(qname string, qtype dnspacket.QueryType) ([]byte, error){
 	}
 	packet.HDR.RD = true
 	packet.HDR.QDCOUNT = 0
-	packet.HDR.Id = 6666 
+	packet.HDR.Id = id 
 	packet.Questions = qs 
 
 	b := buffer.New()
@@ -52,9 +52,7 @@ func lookup(qname string, qtype dnspacket.QueryType) ([]byte, error){
 
 
 func main() {
-	port := flag.String("l", "1234", "Provide listen port")
-
-	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:" + *port)
+	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:1234")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -71,9 +69,12 @@ func main() {
 		}
 		p := dnspacket.FromRaw(buf)
 		// Expecting exactly one question.
+		if len(p.Questions) == 0 {
+			fmt.Println("Nothing in question section.")
+			os.Exit(1)
+		}
 		q := p.Questions[0]
-		// 
-		a, err := lookup(q.Name, q.QType)
+		a, err := lookup(q.Name, q.QType, p.HDR.Id)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -81,73 +82,4 @@ func main() {
 		
 		conn.WriteToUDP(a, addr)
 	}
-
-
-    //serve()
-}
-
-func serve() {
-	qname := "google.com"
-	qtype := dnspacket.QueryType(1)
-
-	// bind udp to arbritrary port
-    // Try without port..
-    udpAddr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
-	qs := []dnspacket.DNSQuestion{
-		{
-			Name:  qname,
-			QType: qtype,
-		},
-	}
-	packet := dnspacket.DNSPacket{
-		HDR:         &dnspacket.DnsHeader{},
-		Questions:   qs,
-	}
-	packet.HDR.RD = true
-	packet.HDR.QDCOUNT = 0
-	packet.HDR.Id = 6666 
-	packet.Questions = qs 
-
-	b := buffer.New()
-	dnspacket.MustWritePacket(b, &packet)
-	// Next just write this packet to the buffer.
-	pInner := b.Inner()
-
-    conn, err := net.DialUDP("udp", nil, udpAddr) 
-
-	if err != nil {
-		fmt.Println(err)
-		panic("couldn't dail")
-	}
-	_, err = conn.Write(pInner[:])
-	_, err = conn.Write([]byte("\n"))
-
-    if err != nil {
-        fmt.Println(err)
-    }
-
-    answerGoogleWrongType := make([]byte, 512) 
-	_, err = bufio.NewReader(conn).Read(answerGoogleWrongType)
-    
-    
-    fmt.Println("recieved something :)")
-
-    // fix this in post :P 
-    answerGoogle := [512]byte{}
-    copy(answerGoogle[:], answerGoogleWrongType)
-    bb := buffer.New()
-
-    bb.SetInner(answerGoogle)
-	returnPacket := dnspacket.New()
-    returnPacket.FromPacketBuffer(bb)
-
-    fmt.Printf("%v",returnPacket.HDR)
-
-    for _, q := range returnPacket.Questions {
-        fmt.Printf("question: %+v\n",q)
-    }
-
-    for _, a := range returnPacket.Answers {
-        fmt.Printf("answer: %+v\n",a)
-    }
 }
